c0813790c3a695351faa487e053abb2d58e66f53
AidenWalmer <aidenjwalmer@gmail.com>
2022-09-29 19:56:02 -0400

    created/styled login page

---
+++ src/Login/loginMain.js
@@ -0,0 +1,19 @@
+/**
+ * Author: Aiden Walmer
+ * Date: 9/29/22
+ * Description: Secure Login, using username and password
+ */
+
+window.addEventListener('load', init);    // When window is loaded, it will call init
+
+function init() {
+
+   let btnElement = document.querySelector('button');   // select the button by query
+   btnElement.addEventListener('click', login);   // handles the button click event 
+}
+
+function login() {
+    
+}
+
+
