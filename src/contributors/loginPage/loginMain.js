f697c2b1e39040dd337b72f955e0408603497be8
AidenWalmer <aidenjwalmer@gmail.com>
2022-09-30 14:55:19 -0400

    login page strucutre and style completed

---
+++ src/contributors/loginPage/loginMain.js
@@ -0,0 +1,25 @@
+/**
+ * Author: Aiden Walmer
+ * Date: 9/29/22
+ * Description: Secure Login, using username and password
+ */
+
+window.addEventListener('load', init);    // when window is loaded, it will call init
+
+function init() {
+ 
+    let btnElement = document.querySelector('button');      // select the button by query
+    btnElement.addEventListener('click', login);            // handles the button click event 
+ 
+    /* Button Color Changes */
+    btnElement.addEventListener('mousemove', () => {        // when hovering over the button, perform hover color change
+       btnElement.classList.add('hover');
+    });
+}
+ 
+function login() {
+     alert("Logged In!")
+}
+ 
+ 
+ 
