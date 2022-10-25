1bdde64e94f4729e53047bcb363cddf78d5f95b0
AidenWalmer <aidenjwalmer@gmail.com>
2022-10-25 10:58:41 -0400

    login page changes and functionality

--- src/loginPage/loginMain.js
+++ src/loginPage/loginMain.js
@@ -5,21 +5,53 @@
  */
 
 window.addEventListener('load', init);    // when window is loaded, it will call init
+var loggedIn = false;
 
 function init() {
  
     let btnElement = document.querySelector('button');      // select the button by query
-    btnElement.addEventListener('click', login);            // handles the button click event 
+    btnElement.addEventListener('click', authenticate);            // handles the button click event 
  
     /* Button Color Changes */
     btnElement.addEventListener('mousemove', () => {        // when hovering over the button, perform hover color change
        btnElement.classList.add('hover');
     });
+
+    function authenticate() {
+        let password = document.getElementById('password').value;
+
+        loggedIn = login(password);
+        status();
+    }
 }
  
-function login() {
-     alert("Logged In!")
-     btnElement.classList.add('clicked');
+function login(password) {
+    var storedPassword = 'foodisgood'
+    return password == storedPassword;
+}
+
+function status() {
+    if (loggedIn) {
+        console.log('You logged in')
+        
+    }
+    else {
+        alert("Incorrect Password!")
+        let password = document.getElementById('password').value;
+        password.innerText = "";
+        var style = document.createElement('style');
+        style.innerHTML = `
+        #passwordLabel, ::placeholder {
+            color: red;
+        }
+
+        #password {
+            border-color: red;
+        }
+        `;
+
+        document.head.appendChild(style);
+    }
 }
  
  
