c6cfebef2ccbf83287f93eb8ef32cf5a20e2902e
AidenWalmer <aidenjwalmer@gmail.com>
2022-10-28 10:20:05 -0400

    fixed login

--- src/loginPage/loginMain.js
+++ src/loginPage/loginMain.js
@@ -22,7 +22,8 @@ function init() {
         let password = document.getElementById('password').value;
         let username = document.getElementById('username').value;
 
-        login(username, password);
+        loggedIn = login(username, password);
+        loginStatus();
     }
 }
  
@@ -36,21 +37,25 @@ function login(username, password) {
     console.log(username);
     console.log(password);
 
-    if (username == storedUsername && password == storedPassword) {
+    if (username == storedUsername) {
+        return password == storedPassword;
+    }
+}
+
+function loginStatus() {
+    if (loggedIn) {
         alert('You logged in!');
         window.location.href = "membersPage/members.html";  // ADD LINK TO ADMIN VERSION OF MEMBERS PAGE!!!!!!!!!!!!!!!!!!
     }
-    else if (username != storedUsername && password != storedPassword) {
+    else {
         console.log('Incorrect username and password!');
-        // Error Message reveal for both username and password
-        passwordErrorDiv.classList.add("hidden");
-        usernameErrorDiv.classList.add("hidden");
-        bothErrorDiv.classList.remove("hidden");
+        // Error Message reveal 
+        errorDiv.classList.remove("hidden");
 
         // Red styliziation around username/password field 
         var style = document.createElement('style');
         style.innerHTML = ` 
-        #usernameLabel, #passwordLabel, #username::placeholder, #password::placeholder {
+        #username::placeholder, #password::placeholder {
             color: red;
         }
         
@@ -60,57 +65,8 @@ function login(username, password) {
         `;
         document.head.appendChild(style);
     }
-    else if (username != storedUsername && password == storedPassword) {
-        console.log('Incorrect username!');
-        // Error Message reveal for username
-        passwordErrorDiv.classList.add("hidden");
-        bothErrorDiv.classList.add("hidden");
-        usernameErrorDiv.classList.remove("hidden");
-
-        // Red styliziation around username 
-        var style = document.createElement('style');
-        style.innerHTML = ` 
-        #usernameLabel, #username::placeholder {
-            color: red;
-        }
-        #username {
-            border-color: red;
-        }
-
-        #passwordLabel {
-            color: black;
-        }
-        #password, #password::placeholder {
-            border-color: black;
-            color: black;
-        }
-        `;
-        document.head.appendChild(style);
-    }
-    else {
-        console.log('Incorrect password!');
-        // Error Message reveal for password
-        usernameErrorDiv.classList.add("hidden");
-        bothErrorDiv.classList.add("hidden");
-        passwordErrorDiv.classList.remove("hidden");
-
-        // Red styliziation around password field 
-        var style = document.createElement('style');
-        style.innerHTML = ` 
-        #passwordLabel, #password::placeholder {
-            color: red;
-        }
-        
-        #password {
-            border-color: red;
-        }
-
-        #usernameLabel, #username::placeholder {
-            color: black;
-        }
-        #username {
-            border-color: black;
-        `;
-        document.head.appendChild(style);
-    }
 }
+ 
+ 
+ 
+ 
