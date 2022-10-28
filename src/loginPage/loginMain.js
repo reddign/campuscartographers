b6b524dc489fcccb67084dff338ba89fcd2576ae
AidenWalmer <aidenjwalmer@gmail.com>
2022-10-27 23:39:53 -0400

    Revamped error messages/highlights in login

--- src/loginPage/loginMain.js
+++ src/loginPage/loginMain.js
@@ -10,7 +10,7 @@ var loggedIn = false;
 function init() {
  
     let btnElement = document.querySelector('button');      // select the button by query
-    btnElement.addEventListener('click', authenticate);     // handles the button click event 
+    btnElement.addEventListener('click', authenticate);     // on button click, redirect to adminMembersPage
  
     /* Button Color Changes */
     btnElement.addEventListener('mousemove', () => {        // when hovering over the button, perform hover color change
@@ -22,53 +22,95 @@ function init() {
         let password = document.getElementById('password').value;
         let username = document.getElementById('username').value;
 
-        loggedIn = login(username, password);
-        loginStatus();
+        login(username, password);
     }
 }
-
-/* Return login if the storedpassword matches the user password
- * Arguments: username and password 
-*/
-function login(username, password) {
-    var storedPassword = 'beans'
-    if (username != "") {
-        return password == storedPassword;
-    }
-}
-
+ 
 /* If login successful, transfer user to new page
- * If login unsuccessful, display error message and reset password field for resubmission
+ * If login unsuccessful, display the correct error message
  */
-function loginStatus() {
-    if (loggedIn) {
-        alert('You logged in!')
-        // window.location.href = "";      ADD LINK TO ADMIN VERSION OF WEBPAGE (EDIT INVENTORY/UPLOAD IMAGES!!!!!!!!!!!!!!
+function login(username, password) {
+    var storedUsername = 'spikeAdmin';
+    var storedPassword = 'spookball4life';
+
+    console.log(username);
+    console.log(password);
+
+    if (username == storedUsername && password == storedPassword) {
+        alert('You logged in!');
+        window.location.href = "membersPage/members.html";  // ADD LINK TO ADMIN VERSION OF MEMBERS PAGE!!!!!!!!!!!!!!!!!!
     }
-    else {
-        console.log('You are not logged in!')
-
-        // Reveal Error Message 
-        errorDiv.classList.remove("hidden");
-
-        // Reset text in password input field 
-        let password = document.getElementById('password').value;
-        password.innerText = "";
+    else if (username != storedUsername && password != storedPassword) {
+        console.log('Incorrect username and password!');
+        // Error Message reveal for both username and password
+        passwordErrorDiv.classList.add("hidden");
+        usernameErrorDiv.classList.add("hidden");
+        bothErrorDiv.classList.remove("hidden");
 
         // Red styliziation around username/password field 
         var style = document.createElement('style');
         style.innerHTML = ` 
-        #passwordLabel, ::placeholder {
+        #usernameLabel, #passwordLabel, #username::placeholder, #password::placeholder {
             color: red;
         }
-
-        #password {
+        
+        #username, #password {
             border-color: red;
         }
         `;
         document.head.appendChild(style);
     }
+    else if (username != storedUsername && password == storedPassword) {
+        console.log('Incorrect username!');
+        // Error Message reveal for username
+        passwordErrorDiv.classList.add("hidden");
+        bothErrorDiv.classList.add("hidden");
+        usernameErrorDiv.classList.remove("hidden");
+
+        // Red styliziation around username 
+        var style = document.createElement('style');
+        style.innerHTML = ` 
+        #usernameLabel, #username::placeholder {
+            color: red;
+        }
+        #username {
+            border-color: red;
+        }
+
+        #passwordLabel {
+            color: black;
+        }
+        #password, #password::placeholder {
+            border-color: black;
+            color: black;
+        }
+        `;
+        document.head.appendChild(style);
+    }
+    else {
+        console.log('Incorrect password!');
+        // Error Message reveal for password
+        usernameErrorDiv.classList.add("hidden");
+        bothErrorDiv.classList.add("hidden");
+        passwordErrorDiv.classList.remove("hidden");
+
+        // Red styliziation around password field 
+        var style = document.createElement('style');
+        style.innerHTML = ` 
+        #passwordLabel, #password::placeholder {
+            color: red;
+        }
+        
+        #password {
+            border-color: red;
+        }
+
+        #usernameLabel, #username::placeholder {
+            color: black;
+        }
+        #username {
+            border-color: black;
+        `;
+        document.head.appendChild(style);
+    }
 }
- 
- 
- 
