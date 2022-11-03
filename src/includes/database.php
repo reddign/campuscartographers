cabf62b7289c11972814d4146e628654e6c76eef
K1ngMatt <suttonm@etown.edu>
2022-11-03 08:43:54 -0400

    database.php finished

--- src/includes/database.php
+++ src/includes/database.php
@@ -1,7 +1,7 @@
 <?PHP
 //connect to the db
-$servername = "156.67.74.65";
-$username = "u413142534.bluejaypantry";
+$servername = "156.67.74.51";
+$username = "u413142534_bluejay";
 $password = "Xdr341Food";
-$database = "dataana8_baseball";
+$database = "u413142534_pantry";
 ?>
