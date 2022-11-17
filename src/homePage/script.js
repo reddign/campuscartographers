2e2c5abcd3537a5822dde0dc731dc1be9f55d62c
[AlexanderFox6] <[foxa1@etown.edu]>
2022-11-17 09:31:18 -0500

    add to cart buttons. commiting to help adam

---
+++ src/homePage/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    window.addEventListener('load', init);
+
+    function init(){
+        let addCartBtn = document.querySelectorAll('.addCart');
+        addCartBtn.addEventListener('click', addCart);
+    }
+
+    function addCart(){
+        
+    }
+
+
+
+})();
